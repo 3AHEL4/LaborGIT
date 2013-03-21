47f9551b698338dcd2e5387432af9d73090d66fb
3AHELX <matthias.schorn@htl-klu.at>
2013-03-21 11:27:55 +0100

    Aufgabe erledigt !

--- GIT Project/src/GIT/GITProject.java
+++ GIT Project/src/GIT/GITProject.java
@@ -1,5 +1,7 @@
 package GIT;
 
+import java.util.Random;
+
 public class GITProject {
 	/* int Array anlegen - Wurzer
 	 * Werte zuordnen - Schorn
@@ -10,12 +12,21 @@ public class GITProject {
 	 * rotateRight - Widmann
 	 */
 	
-	int b [] = new int[10];
 	
+	public int b [] = new int[10];
+	
+	public void werteGeben(int x){
+
+		Random r = new Random();
+		int i =0;
+		for(; i <=10;i++){
+			b[i]=r.nextInt(x);
+		}
+		System.out.println(""+b[i]);
+	}
 	
 	public static void main(String[] args) {
 		//		Das ist ein Programm, das über GIT mit anderen Usern geteilt wird.
-		
 		
 	}
 
