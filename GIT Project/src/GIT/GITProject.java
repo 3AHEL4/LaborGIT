93d85402799bd82a940fa51e9c90ea219ce37d8d
3503040796 <3503040796@PC55.el.htl.local>
2013-03-21 11:12:38 +0100

    Aufgaben vergeben

--- GIT Project/src/GIT/GITProject.java
+++ GIT Project/src/GIT/GITProject.java
@@ -1,7 +1,14 @@
 package GIT;
 
 public class GITProject {
-
+	/* int Array anlegen - Wurzer
+	 * Werte zuordnen - Schorn
+	 * Methode, die die Summer aller Werte bildet - Wagger
+	 * shiftLeft - Schöffmann
+	 * shiftRight - Stocker
+	 * rotateLeft - Steiner
+	 * rotateRight - Widmann
+	 */
 	
 	public static void main(String[] args) {
 		//		Das ist ein Programm, das über GIT mit anderen Usern geteilt wird.
