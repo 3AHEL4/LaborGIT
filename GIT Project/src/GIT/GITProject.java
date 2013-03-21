95ec448d2a993ac06442e9ca05fa9590ed4d7861
4849170795 <4849170795@PC57.el.htl.local>
2013-03-21 10:22:40 +0100

    GIT Server

--- GIT Project/src/GIT/GITProject.java
+++ GIT Project/src/GIT/GITProject.java
@@ -5,12 +5,14 @@ public class GITProject {
 	
 	public static void main(String[] args) {
 		int c = 10+20;
-		System.err.println(c+"	");
-		System.out.println("Das wurde verändert -Alex");
-		System.out.println("Das wurde verändert -Steiner");
-		System.out.println("Das wurde verändert -Schöffmann");
-		System.out.println("Das wurde verändert -Stocker");
-		System.out.println("I habs geschafft ;)) -Schorn");
+		System.err.println(c+"???");
+		System.out.println("Das wurde verändert 	-Alex");
+		System.out.println("Das wurde verändert 	-Steiner");
+		System.out.println("Das wurde verändert 	-Schöffmann");
+		System.out.println("Das wurde verändert 	-Stocker");
+		System.out.println("I habs geschafft ;))	-Schorn");
+		System.out.println("Scheiß Combo Braker 	-Schöffmann");
+		
 	}
 
 }
