3038ff42c9f92b6dc9fe0dff045b026c3101f17c
4849170795 <4849170795@PC57.el.htl.local>
2013-03-21 10:13:45 +0100

    Wieder einmal verändert ^^

--- GIT Project/src/GIT/GITProject.java
+++ GIT Project/src/GIT/GITProject.java
@@ -5,7 +5,7 @@ public class GITProject {
 	
 	public static void main(String[] args) {
 		int c = 10+20;
-		System.err.println(c+"???");
+		System.err.println(c+"	");
 		System.out.println("Das wurde verändert -Alex");
 		System.out.println("Das wurde verändert -Steiner");
 		System.out.println("Das wurde verändert -Schöffmann");
